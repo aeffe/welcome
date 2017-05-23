c92e3a156ee1e695bf19edc3c3c521b1a76217d5
uja00134 <andrea.franzese@unipolsai.it>
2017-05-23 18:02:26 +0200

    Implementing login control

--- welcome-client/src/app/app.module.ts
+++ welcome-client/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdInputModule, MdButtonModule } from '@angular/material';
+import { MdInputModule, MdButtonModule, MdCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
